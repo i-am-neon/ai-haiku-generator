82d6644f98e46f026756bd5ba82bdc91b1797a3a
0xNeon <anonymous_crypto_person@protonmail.com>
2021-10-30 17:32:51 -0400

    fixed cors

--- source/server.ts
+++ source/server.ts
@@ -17,10 +17,10 @@ router.use((req, res, next) => {
     // set the CORS policy
     res.header('Access-Control-Allow-Origin', '*');
     // set the CORS headers
-    res.header('Access-Control-Allow-Headers', 'origin, X-Requested,Content-Type,Accept, Authorization');
+    res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With,Content-Type,Accept, Authorization');
     // set the CORS method headers
     if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'GET PATCH PUT DELETE POST');
+        res.header('Access-Control-Allow-Methods', '*');
         return res.status(200).json({});
     }
     next();
