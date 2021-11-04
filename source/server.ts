1b0b09a68b5b5f7cb9a17150429e58c2fcad1441
0xNeon <anonymous_crypto_person@protonmail.com>
2021-11-04 13:55:46 -0400

    changed back CORS

--- source/server.ts
+++ source/server.ts
@@ -43,7 +43,8 @@ router.use(express.json());
 /** RULES OF OUR API */
 router.use((req, res, next) => {
     // set the CORS policy
-    res.header('Access-Control-Allow-Origin', process.env.ALLOW_ORIGIN);
+    res.header('Access-Control-Allow-Origin', '*');
+    // res.header('Access-Control-Allow-Origin', process.env.ALLOW_ORIGIN);
     // set the CORS headers
     res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With,Content-Type,Accept, Authorization');
     // set the CORS method headers
