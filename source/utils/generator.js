45ef6bcdb54e250f65ed32921d09947d9a3a8e3e
0xNeon <anonymous_crypto_person@protonmail.com>
2021-11-10 22:24:35 -0500

    added fonts 🔡

--- source/utils/generator.js
+++ source/utils/generator.js
@@ -1,9 +1,20 @@
-// import oguraHempFibers from "./source/assets/papers/ogura-hemp-fibers.jpeg";
-import { createCanvas, loadImage } from 'canvas';
+import { createCanvas, loadImage, registerFont } from 'canvas';
 import fs from 'fs';
+
 export const generateHaiku = async (haikuTitle, haikuContent) => {
     console.log('haikuText :>> ', haikuContent);
 
+    // Registering fonts
+
+    // Sans serif
+    registerFont('source/assets/fonts/Comfortaa-VariableFont_wght.ttf', { family: 'Comfortaa' });
+    registerFont('source/assets/fonts/MontserratAlternate.ttf', { family: 'Montserrat Alternate' });
+    
+    // Serif
+    registerFont('source/assets/fonts/YujiSyuku.ttf', { family: 'Yuji Syuko' });
+    registerFont('source/assets/fonts/Cardo-Bold.ttf', { family: 'Cardo Bold' });
+    registerFont('source/assets/fonts/Eczar.ttf', { family: 'Eczar' });
+    
     // Haiku papers will be 10.5cm x 14.85cm
     const width = 1050
     const height = 1485
@@ -11,26 +22,25 @@ export const generateHaiku = async (haikuTitle, haikuContent) => {
     const canvas = createCanvas(width, height)
     const context = canvas.getContext('2d')
 
-    const imageAddress = 'source/assets/papers/gold-and-silver.jpeg';
-    // const imageAddress = 'https://cdn.shopify.com/s/files/1/2472/3474/files/product_f52_gallery02.jpg?v=1587317134';
+    const imageAddress = 'source/assets/papers/sample-background.png';
     const image = await loadImage(imageAddress);
-    
+
     context.drawImage(image, 0, 0, width, height)
-    context.font = 'bold 24pt Menlo'
+    context.font = '36pt "Zen Kurenaido"'
     context.textAlign = 'center'
     context.textBaseline = 'top'
     context.fillStyle = '#3574d4'
 
     const haikuLines = haikuContent.split('\n');
     const lineHeight = context.measureText(haikuLines[0]).emHeightDescent;
-    const startHaikuContentHeightFromTop = height * .5;
+    const startHaikuContentHeightFromTop = height * .55;
 
     context.fillStyle = '#000'
 
     context.fillText(haikuLines[0], width / 2, startHaikuContentHeightFromTop)
     context.fillText(haikuLines[1], width / 2, startHaikuContentHeightFromTop - 2 * lineHeight)
     context.fillText(haikuLines[2], width / 2, startHaikuContentHeightFromTop - 4 * lineHeight)
-    
+
     const buffer = canvas.toBuffer('image/png')
     fs.writeFileSync('./source/assets/output/test.png', buffer)
 
