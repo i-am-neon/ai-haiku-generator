de94be6b37025b5c7305ed980f145e436d873182
the0xcollective <90662438+0xNeon-opensource@users.noreply.github.com>
2021-11-28 10:59:21 -0600

    update whitelist

--- source/utils/whitelist.ts
+++ source/utils/whitelist.ts
@@ -1,27 +1,29 @@
-export const WHITELIST_MINT_TIMESTAMP_MS = 0;
-// export const WHITELIST_MINT_TIMESTAMP_MS = 1638147600000;
-export const PUBLIC_MINT_TIMESTAMP_MS = 0;
-// export const PUBLIC_MINT_TIMESTAMP_MS = 1638151200000;
+// export const WHITELIST_MINT_TIMESTAMP_MS = 0;
+export const WHITELIST_MINT_TIMESTAMP_MS = 1638147600000;
+// export const PUBLIC_MINT_TIMESTAMP_MS = 0;
+export const PUBLIC_MINT_TIMESTAMP_MS = 1638151200000;
 
 export const whitelistedAddresses = [
     // NEON:
     '0x915D8f12f523273c814D51e29962fdcb49ad36d5',
+    // NEON account 2:
+    '0x4C164132501908f5351970214A2A6E755A949A53',
     // Nadia:
     '0x263Ea7Aeb309B7DA0eE2679B5b8DD76D56AB455e',
     // ImTheCook:
     '0x1BB7615a17a795b3d79cdA72467aD2aBAF53F2ab',
-    // :
-    '',
-    // :
-    '',
-    // :
-    '',
-    // :
-    '',
-    // :
-    '',
-    // :
-    '',
+    // crypt_SA | DinoPunks:
+    '0x171C99CC536DE488D1FdA43e604E31A4c38aAE6A',
+    // non-fungi:
+    '0x9361C1Bbe1c7E3197758414B49D19BcE1241668A',
+    // Devin:
+    '0x21A016a6A8b87907bB7A6926a6DC1905789F812f',
+    // Blockbrain:
+    '0xfedCa5322438bdA8C535f1aCb00Fb2f1A0536149',
+    // Quil:
+    '0x86c28dB1118c55Ba85C562D5b0875f94d78B0C03',
+    // TheTonyD:
+    '0x6Ab63d3eff6DD73F0C68d26ef52CB50CDB757F57',
     // :
     '',
     // :
